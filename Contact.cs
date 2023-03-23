f389cf8ac1989ed4c80164e2ae290d2c495da648
arivusurya <arivusurya.3@gmail.com>
2023-03-23 21:29:19 +0530

    addressbook using linq

---
+++ Contact.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+
+namespace AddressBook{
+    public class Contact{
+        public int id {get;set;}
+        public string firstname {get;set;}
+        public string lastname {get;set;}
+        public string City {get;set;}
+        public string State {get;set;}
+        public Int64  phone {get;set;}
+        public int zip {get;set;}
+        public string email {get;set;} 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+namespace LINQAddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LINQ AddressBookSystem!");
+            //Create Object for DataTable
+            ContactDataManager contactDataManager = new ContactDataManager();
+            ContactDataManager contactDataManagers = new ContactDataManager();
+            DataTableManager dataTableManger = new DataTableManager();
+            dataTableManger.CreateDataTable();
+
+            //Insert Values into Table
+            contactDataManager.FirstName = "Shalini";
+            contactDataManager.LastName = "Venkatesh";
+            contactDataManager.PhoneNumber = 9842905050;
+            contactDataManager.Email = "shalini@gmail.com";
+            contactDataManager.Address = "4,B Block,Avadi";
+            contactDataManager.City = "chennai";
+            contactDataManager.State = "TN";
+            contactDataManager.zip = 600072;
+            dataTableManger.InsertintoDataTable(contactDataManager);
+
+            //Modify
+            int varl = dataTableManger.EditDataTable("lalita", "Lastname");
+            Console.WriteLine("Success" + varl);
+            //Delete
+            int var2 = dataTableManger.DeleteRowInDataTable("lalita");
+            Console.WriteLine("Success" + varl);
+            //Retrieve based on city or state
+            string var3 = dataTableManger.RetrieveBasedOnCityorState("Bareilly", "UP");
+            Console.WriteLine("Success" + varl);
+            //count based on city or state
+            string var4 = dataTableManger.RetrieveCountBasedOnCityorState();
+            Console.WriteLine("Success" + varl);
+            //sort based on name in data table
+            string var5 = dataTableManger.SortBasedOnNameInDataTable("chennai");
+            Console.WriteLine("Success" + varl);
+        }
+    }
+}
